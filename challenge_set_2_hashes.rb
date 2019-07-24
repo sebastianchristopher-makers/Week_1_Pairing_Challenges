b31cb6e61efb9770ca509e6a0d10c2e103336b62
sebastianchristopher <christophersebastianjohnson@gmail.com>
2019-07-24 08:31:57 +0100

    first commit, 2 challneges complete

---
+++ challenge_set_2_hashes.rb
@@ -0,0 +1,28 @@
+=begin
+Challenge Set 2: Hashes
+
+Transform the following first hashes into the second entities:
+
+{ a: 1, b: 2 } => { a: 2, b: 3 }
+{ a: 2, b: 5, c: 1 } => { a: 1, b: 2, c: 5 }
+{ a: 2, b: 5, c: 1 } => [1, 2, 5]
+{ a: 1, b: 2, c: 3 } => { "1"=>"a", "2"=>"b", "3"=>"c" }
+=end
+
+class Test
+  def self.assert_equals(expected,actual)
+    expected == actual
+  end
+end
+
+puts "1. { a: 1, b: 2 } => { a: 2, b: 3 }"
+h = { a: 1, b: 2 }
+expected = { a: 2, b: 3 }
+try = h.map{|k,v|[k,v+=1]}.to_h
+puts Test.assert_equals(expected,try)
+
+puts "2. { a: 2, b: 5, c: 1 } => { a: 1, b: 2, c: 5 }"
+h = { a: 2, b: 5, c: 1 }
+expected = { a: 1, b: 2, c: 5 }
+try = h.keys.zip(h.values.sort).to_h
+puts Test.assert_equals(expected,try)
